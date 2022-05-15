cc723d95a893f9fe4921b9a5c6f2ac53b1f404b5
SotskovS <svalgele@gmail.com>
2022-05-15 13:48:45 +0300

    task 1.1

--- Week_1/DZ_1.java
+++
@@ -1,6 +0,0 @@
-public class VariablesTheme {
-    
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ Week_1/VariablesTheme.java
@@ -0,0 +1,18 @@
+public class VariablesTheme {
+    
+    public static void main(String[] args) {
+        byte bitness = 64;
+        short cointMonitor = 2;
+        int codeProduct = 00326; 
+        long buildOS = 190441706L;
+        float wightPC = 10.2f;
+        double weightMouse = 0.127;        
+        char namePC = 'a';
+        boolean windowsOS = true;
+
+        System.out.println("bitness: " + bitness + "\n" + "cointMonitor: " + cointMonitor + "\n" +
+                           "codeProduct: " + codeProduct + "\n" + "buildOS: " + buildOS + "\n" +
+                           "wightPC: " + wightPC + "\n" + "weightMouse: " + weightMouse + "\n" +
+                           "namePC: " + namePC + "\n" + "windowsOS: " + windowsOS + "\n");
+    }
+}
