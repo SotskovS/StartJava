89d2f168738a7eaed8c29e00196891edf69d920e
SotskovS <svalgele@gmail.com>
2022-06-09 00:54:59 +0300

    Task(if): 4

--- Week_1/IfElseStatementTheme.java
+++ Week_1/IfElseStatementTheme.java
@@ -63,6 +63,48 @@ public class IfElseStatementTheme {
 
         System.out.println("");
 
-        
+        numOne = 104;
+        int numOne_X = numOne / 100;
+        int numOne_Y = numOne % 100 / 10;
+        int numOne_Z = numOne % 10;
+        numTwo = 154;
+        int numTwo_X = numTwo / 100;
+        int numTwo_Y = numTwo % 100 / 10;
+        int numTwo_Z = numTwo % 10;
+        boolean a = false;
+        boolean b = false;
+        boolean c = false;
+
+        if (numOne_X == numTwo_X) { a = true; }
+        if (numOne_Y == numTwo_Y) { b = true; }
+        if (numOne_Z == numTwo_Z) { c = true; }
+
+        if (a && b && c) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры " + numOne_X + ", " + numOne_Y + ", " +
+                numOne_Z + " стоят в 1, 2, 3 разрядах");
+        } else if (a && b) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры " + numOne_X + ", " + numOne_Y +
+                " стоят в 1, 2 разрядах");            
+        } else if (b && c) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры " + numOne_Y + ", " + numOne_Z +
+                " стоят в 2, 3 разрядах");            
+        } else if (a && c) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры " + numOne_X + ", " + numOne_Z +
+                " стоят в 1, 3 разрядах");            
+        } else if (a) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры \"" + numOne_X + "\" стоят в 1 разряде");            
+        } else if (b) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры \"" + numOne_Y + "\" стоят в 2 разряде");            
+        } else if (c) {
+            System.out.println("В числах " + numOne + " и " + numTwo + 
+                " одинаковые цифры \"" + numOne_Z + "\" стоят в 3 разряде");            
+        } 
+
     }
 }
