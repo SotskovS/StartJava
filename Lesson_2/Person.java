cbc32ed1c8a902b086c3a568859f69bb670b6e32
SotskovS <svalgele@gmail.com>
2022-07-26 23:13:49 +0300

    Task(class): 1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String gender = "male";
+    String name = "Mark";
+    int height = 175;
+    float weight = 77.2f;
+    int age = 37;
+
+    public void move() {
+        System.out.println("Человек идет");
+    }
+
+    public void sit () {
+        System.out.println("Человек сидит");
+    }
+
+    public void run() {
+        System.out.println("Человек бежит");
+    }
+
+    public void say() {
+        System.out.println("Человек говорит");
+    }
+
+    public void learn() {
+        System.out.println("Человек учит Java");    
+    }
+}
