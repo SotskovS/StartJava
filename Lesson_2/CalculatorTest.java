daed2a4de607b677f8c70af53d72a8434e7a1e10
SotskovS <svalgele@gmail.com>
2022-08-04 23:34:26 +0300

    Task(lesson_2) total_Calculator

---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        String userAnswer;
+
+        do {
+            System.out.print("Введите первое число: ");
+            int a = scan.nextInt();
+            calc.setA(a);
+            
+            System.out.print("Введите знак математической операции: ");
+            char sign = scan.next().charAt(0);
+            calc.setSign(sign);
+            
+            System.out.print("Введите второе число: ");
+            int b = scan.nextInt();
+            calc.setB(b);
+
+            System.out.println(calc.getResult());
+
+            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+            userAnswer = scan.next();
+
+            if (!userAnswer.equals("no") && !userAnswer.equals("yes")) {
+                boolean flag;
+                do {
+                    System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+                    userAnswer = scan.next();
+                    if (userAnswer.equals("no")) {
+                        flag = false;
+                    } else if(userAnswer.equals("yes")) {
+                        flag = false;
+                    } else {
+                        flag = true;
+                    }
+                } while (flag);
+            } 
+         } while(!userAnswer.equals("no"));
+    }
+}
