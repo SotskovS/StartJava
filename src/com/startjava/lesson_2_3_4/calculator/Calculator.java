c2c3946489b03926756256a1f6d85e8a6527cfef
SotskovS <svalgele@gmail.com>
2022-09-22 23:34:24 +0300

    arrays: calc *

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,28 +1,33 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-
-    private int a;
-    private int b;
-    private char sign;
-
-    public int calculate(String userExpression) {
+    public static int calculate(String userExpression) {
         String[] partsExpression = userExpression.split(" ");
-        a = Integer.parseInt(partsExpression[0]);
-        sign = partsExpression[1].charAt(0);
-        b = Integer.parseInt(partsExpression[2]);
 
-        if (b == 0) {
-            System.out.println("На 0 делить нельзя");            
-        } else {
-            switch (sign) {
-                case '+': return a + b;
-                case '-': return a - b;
-                case '*': return a * b;
-                case '/': return a / b;
-                case '%': return a % b;
-                case '^': return (int) Math.pow(a, b);
+        int a = 0;
+        char sign = partsExpression[1].charAt(0);
+        int b = 0;
+
+        try {
+            a = Integer.parseInt(partsExpression[0]);
+            b = Integer.parseInt(partsExpression[2]);
+            if (a < 0 || b < 0) {
+                throw new Exception();
             }
+            if ((sign == '/' && b == 0)) {
+                System.out.println("На 0 делить нельзя!");
+            } else {
+                switch (sign) {
+                    case '+': return a + b;
+                    case '-': return a - b;
+                    case '*': return a * b;
+                    case '%': return a % b;
+                    case '^': return (int) Math.pow(a, b);
+                    case '/': return (b == 0 ? 0 : a / b);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Введены не корректные значения");
         }
         return 0;
     }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
 
         String answer = "yes";
         do {
             if ("yes".equals(answer)) {
-                System.out.println("Введите выражение в формате - \"a + b\": ");
+                System.out.println("Введите выражение в формате - \"a + b\" (число должно быть целым и положительным)");
                 String mathExpression = scan.nextLine();
-                System.out.println(calc.calculate(mathExpression));
+                System.out.println(Calculator.calculate(mathExpression));
             }
 
             System.out.print("Хотите продолжить вычисления? [yes/no]: ");
